7b863c3e2e45fb6acc043acbd61f736b5bf399a4
jimmyjawnz <jamesprovee@gmail.com>
2025-05-12 18:18:17 -0600

    Customer Entity Added

---
+++ BakeryManagementPOSWebApp/Data/Enities/Abstractions/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BakeryManagementPOSWebApp.Data.Enities.Abstractions
+{
+    public class Customer : Entity
+    {
+        [Length(1, 25, ErrorMessage = "First name is too large.")]
+        public string FirstName { get; set; } = string.Empty;
+        [Length(1, 25, ErrorMessage = "Last name is too large.")]
+        public string LastName { get; set; } = string.Empty;
+        [Phone(ErrorMessage = "Phone number is not a valid/supported number.")]
+        [Required(ErrorMessage = "Phone number is required.")]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [EmailAddress]
+        public string EmailAddress { get; set; } = string.Empty;
+    }
+}
