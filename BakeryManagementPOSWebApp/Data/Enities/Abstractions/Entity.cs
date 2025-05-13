1e51d0900f16af59c78bea69da2a1dedba13049b
jimmyjawnz <jamesprovee@gmail.com>
2025-05-12 18:17:27 -0600

    Entity Abstraction Added.

---
+++ BakeryManagementPOSWebApp/Data/Enities/Abstractions/Entity.cs
@@ -0,0 +1,39 @@
+﻿namespace BakeryManagementPOSWebApp.Data.Enities.Abstractions
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime? DateUpdated { get; set; }
+        public DateTime? DateDeleted { get; set; }
+
+        public string DateCreatedStr
+        {
+            get
+            {
+                return DateCreated.ToString();
+            }
+        }
+        public string DateUpdatedStr
+        {
+            get
+            {
+                if (DateUpdated is null)
+                {
+                    return "None";
+                }
+                else
+                {
+                    return DateUpdated.ToString()!;
+                }
+            }
+        }
+        public string DateDeletedStr
+        {
+            get
+            {
+                return DateDeleted.ToString()!;
+            }
+        }
+    }
+}
