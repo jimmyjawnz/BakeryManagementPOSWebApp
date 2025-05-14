b174f3eabc83688024bf8cb3154d4e9382554964
jimmyjawnz <jamesprovee@gmail.com>
2025-05-13 18:55:02 -0600

    Created Employee Service. Update Employee Entity to support service.

--- BakeryManagementPOSWebApp/Data/Enities/Employee.cs
+++ BakeryManagementPOSWebApp/Data/Enities/Employee.cs
@@ -32,5 +32,38 @@ public class Employee : IdentityUser
 
     public int CustomerId { get; set; }
     public Customer? Customer { get; set; }
+
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    public DateTime? DateUpdated { get; set; }
+    public DateTime? DateDeleted { get; set; }
+
+    public string DateCreatedStr
+    {
+        get
+        {
+            return DateCreated.ToString();
+        }
+    }
+    public string DateUpdatedStr
+    {
+        get
+        {
+            if (DateUpdated is null)
+            {
+                return "None";
+            }
+            else
+            {
+                return DateUpdated.ToString()!;
+            }
+        }
+    }
+    public string DateDeletedStr
+    {
+        get
+        {
+            return DateDeleted.ToString()!;
+        }
+    }
 }
 
---
+++ BakeryManagementPOSWebApp/Services/Employees/EmployeeService.cs
@@ -0,0 +1,70 @@
+﻿using BakeryManagementPOSWebApp.Data;
+using BakeryManagementPOSWebApp.Data.Enities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BakeryManagementPOSWebApp.Services.Employees
+{
+    public class EmployeeService
+    {
+        readonly ApplicationDbContext _dbContext;
+        public EmployeeService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // Entity Specific
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            return await _dbContext.Employees.ToListAsync();
+        }
+        public async Task<List<Employee>> GetExistingEmployees()
+        {
+            return await _dbContext.Employees.Where(p => p.DateDeleted == null).ToListAsync();
+        }
+        public async Task<List<Employee>> GetTrashedEmployees()
+        {
+            return await _dbContext.Employees.Where(p => p.DateDeleted != null).ToListAsync();
+        }
+
+        public async Task<int> CreateEmployee(Employee Employee)
+        {
+            await _dbContext.Employees.AddAsync(Employee);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Employee> GetEmployee(string id)
+        {
+            return await _dbContext.Employees.Where(p => p.Id == id).FirstAsync();
+        }
+
+        public async Task<int> UpdateEmployee(Employee employee)
+        {
+            employee.DateUpdated = DateTime.Now;
+
+            _dbContext.Employees.Update(employee);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> SoftDeleteEmployee(Employee employee)
+        {
+            employee.DateDeleted = DateTime.Now;
+
+            _dbContext.Employees.Update(employee);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> RecoverEmployee(Employee employee)
+        {
+            employee.DateDeleted = null;
+
+            _dbContext.Employees.Update(employee);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> HardDeleteEmployee(Employee employee)
+        {
+            _dbContext.Employees.Remove(employee);
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
