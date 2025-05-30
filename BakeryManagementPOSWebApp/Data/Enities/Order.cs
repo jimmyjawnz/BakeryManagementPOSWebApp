ada266dc76eaf5fe938564ce20a429c2b50b9f77
jimmyjawnz <jamesprovee@gmail.com>
2025-05-29 20:54:37 -0600

    Updated Orders Entity to save needed information like item total, discount amount, and total amounts in the database. Implemented and Updated related pages as well

--- BakeryManagementPOSWebApp/Data/Enities/Order.cs
+++ BakeryManagementPOSWebApp/Data/Enities/Order.cs
@@ -6,45 +6,45 @@ namespace BakeryManagementPOSWebApp.Data.Enities
 {
     public class Order : Entity
     {
+        // Customer who ordered
         [Required(ErrorMessage = "A valid Customer must be selected.")]
         public int? CustomerID { get; set; }
         public Customer? OrderedBy { get; set; }
 
+        // Employee who processed
         public string? EmployeeID { get; set; }
         public Employee? ProcessedBy { get; set; }
 
+        // Order items linked to the order
         public ICollection<OrderItem>? OrderItems { get; set; } = [];
 
+        // Payment type (Cash, Credit, etc.)
         [Required(ErrorMessage = "A valid Payment Type is required.")]
         public string? PaymentType { get; set; }
 
-        public decimal Discount { get; set; } = 0.00m;
+        // Price sum of order items
+        public decimal SumOfItems { get; set; } = 0.00m;
 
+        // Discount percent and calculated amount
+        public int DiscountPercent { get; set; } = 0;
+        public decimal DiscountAmount { get; set; } = 0.00m;
+
+        // Total order amount
+        public decimal TotalAmount { get; set; } = 0.00m;
+
+        // Order notes
         public string? Notes { get; set; }
 
+        // Planned pickup date
         public DateTime? PickupDate { get; set; }
 
+        // Date Pickedup
+        public DateTime? DatePickedUp { get; set; }
+
+        // Date Processed
         public DateTime? DateProcessed { get; set; }
 
-        public decimal TotalSum { 
-            get
-            {
-                decimal total = 0.0m;
-
-                if (OrderItems is null)
-                {
-                    return total;
-                }
-
-                foreach (OrderItem item in OrderItems)
-                {
-                    total += item.RowPrice;
-                }
-
-                return total;
-            } 
-        }
-
+        // Creates a string with "ID-CustomerID"
         public string OrderIdentifier
         {
             get
@@ -53,6 +53,8 @@ namespace BakeryManagementPOSWebApp.Data.Enities
             }
         }
 
+        // Returns true if the order is processed and picked up,
+        // false if not proccessed and picked up, and null otherwise
         public bool? IsComplete
         {
             get
@@ -72,6 +74,7 @@ namespace BakeryManagementPOSWebApp.Data.Enities
             }
         }
 
+        // Date strings
         public string PickupDateStr
         {
             get
@@ -100,6 +103,36 @@ namespace BakeryManagementPOSWebApp.Data.Enities
                     return DateProcessed.ToString()!;
                 }
             }
+        }
+
+        // Functions
+        public decimal CalcTotalAmount()
+        {
+            TotalAmount = CalcItemsAmount() - CalcDiscountAmount();
+            return TotalAmount;
+        }
+
+        public decimal CalcDiscountAmount()
+        {
+            DiscountAmount = CalcItemsAmount() * (DiscountPercent / 100.0m);
+            return DiscountAmount;
+        }
+
+        public decimal CalcItemsAmount()
+        {
+            SumOfItems = 0.00m;
+
+            if (OrderItems is null || OrderItems.Count <= 0)
+            {
+                return SumOfItems;
+            }
+
+            foreach (var item in OrderItems)
+            {
+                SumOfItems += item.RowPrice;
+            }
+            
+            return SumOfItems;
         }
     }
 }
--- BakeryManagementPOSWebApp/Data/Enities/OrderItem.cs
+++ BakeryManagementPOSWebApp/Data/Enities/OrderItem.cs
@@ -10,9 +10,11 @@ namespace BakeryManagementPOSWebApp.Data.Enities
         [Required(ErrorMessage = "Product is required.")]
         public int ProductId { get; set; }
         public Product? Product { get; set; }
+        public decimal? ProductPrice { get; set; }
+
 
         [Required]
-        public decimal RowPrice { get; set; }
+        public decimal RowPrice { get; set; } = 0.00m;
 
         [Required]
         public string ProductName { get; set; } = string.Empty;
@@ -20,5 +22,12 @@ namespace BakeryManagementPOSWebApp.Data.Enities
         [Required(ErrorMessage = "No Order Relation.")]
         public int OrderId { get; set; }
         public Order? Order { get; set; }
+
+        // Fucntions
+        public decimal CalcRowPrice()
+        {
+            RowPrice = ProductPrice!.Value * Quantity;
+            return RowPrice;
+        }
     }
 }
