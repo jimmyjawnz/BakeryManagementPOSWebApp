e4904a356ab2fa2b98ccb420f6e6389ac60ba121
jimmyjawnz <jamesprovee@gmail.com>
2025-08-23 17:09:57 -0600

    Created DbContext Service that implements a DbFactory

---
+++ BakeryManagementPOSWebApp/Services/DatabaseContextService.cs
@@ -0,0 +1,31 @@
+﻿using BakeryManagementPOSWebApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BakeryManagementPOSWebApp.Services
+{
+    public class DatabaseContextService
+    {
+        readonly IDbContextFactory<ApplicationDbContext> _dbFactory;
+
+        public DatabaseContextService(IDbContextFactory<ApplicationDbContext> dbFactory)
+        {
+            _dbFactory = dbFactory;
+        }
+
+        public async Task<ApplicationDbContext?> CreateDbContext()
+        {
+            try
+            {
+                return await _dbFactory.CreateDbContextAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[ERROR] -> Unable to create Database Context:");
+                Console.WriteLine(ex);
+                Console.ForegroundColor = ConsoleColor.White;
+                return null;
+            }
+        }
+    }
+}
